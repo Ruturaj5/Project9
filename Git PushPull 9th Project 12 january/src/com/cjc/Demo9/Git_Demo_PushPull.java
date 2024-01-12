37224f681f4390af4c9f0780f81ffa8c660f22ff
rutur <rutur@192.168.1.71>
2024-01-12 13:18:09 +0530

    By Ruturaj

---
+++ Git PushPull 9th Project 12 january/src/com/cjc/Demo9/Git_Demo_PushPull.java
@@ -0,0 +1,19 @@
+package com.cjc.Demo9;
+
+public class Git_Demo_PushPull 
+{
+
+	public void ruturaj() 
+	{
+	System.out.println("This is rututraj method we will 1st Push this");	
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("main Method");	
+		Git_Demo_PushPull g9 = new Git_Demo_PushPull ();
+		g9.ruturaj();
+	}
+
+}
