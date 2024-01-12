194575a3e22ffc8cdb71e02f70516557da63fc50
Ruturaj5 <156278942+Ruturaj5@users.noreply.github.com>
2024-01-12 13:20:09 +0530

    Update Git_Demo_PushPull.java

--- Git PushPull 9th Project 12 january/src/com/cjc/Demo9/Git_Demo_PushPull.java
+++ Git PushPull 9th Project 12 january/src/com/cjc/Demo9/Git_Demo_PushPull.java
@@ -3,11 +3,14 @@ package com.cjc.Demo9;
 public class Git_Demo_PushPull 
 {
 
-	public void ruturaj() 
+	public void ruturaj0() 
 	{
-	System.out.println("This is rututraj method we will 1st Push this");	
+	System.out.println("This is rututraj0 method we will 1 Push this normally without Branch");	
 	}
-	
+	public void ruturaj1() 
+	{
+	System.out.println("This is rututraj1 method we will  Push this normally without Branch");	
+	}
 	
 	public static void main(String[] args) 
 	{
