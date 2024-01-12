ce7a134a38af24d065c893a8142cf44b3ea66ab7
Ruturaj5 <156278942+Ruturaj5@users.noreply.github.com>
2024-01-12 13:29:27 +0530

    By rutu
    normal pull

--- Git PushPull 9th Project 12 january/src/com/cjc/Demo9/Git_Demo_PushPull.java
+++ Git PushPull 9th Project 12 january/src/com/cjc/Demo9/Git_Demo_PushPull.java
@@ -5,11 +5,11 @@ public class Git_Demo_PushPull
 
 	public void ruturaj0() 
 	{
-	System.out.println("This is rututraj0 method we will 1 Push this normally without Branch");	
+	System.out.println("This is rututraj0 method we will 1 Push this normally without Branchh");	
 	}
 	public void ruturaj1() 
 	{
-	System.out.println("This is rututraj1 method we will  Push this normally without Branch");	
+	System.out.println("This is rututraj1 method we will  Push this normally without Branchh");	
 	}
 	
 	public static void main(String[] args) 
